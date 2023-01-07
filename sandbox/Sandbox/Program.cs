b425ed12efb37263c1eb38859fb55186a015d998
LukeBenjaminBullock <bul22006@byui.edu>
2023-01-07 15:51:14 -0700

    Finished Prep 1

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        Console.Write("What is your favorite color? ");
+
+        string color = Console.ReadLine(); 
+        Console.WriteLine($"Your Color is {color}");
+        
     }
 }
