32c620b725df9dc83fd2f55612306744a1fb0a70
LukeBenjaminBullock <bul22006@byui.edu>
2023-01-07 17:21:29 -0700

    Prep 3 Completed

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random(); 
+        int magicNumber; 
+
+        string input;
+        int inputInt;
+        int guessCount = 0;
+        string answer = "yes";
+
+        do {
+            magicNumber = randomGenerator.Next(1,100);
+            guessCount = 0; 
+            do {
+                guessCount ++;
+                Console.Write("What is your guess? ");
+                input = Console.ReadLine();
+                inputInt = int.Parse(input);
+
+                if (inputInt < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (inputInt > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine($"It only took you {guessCount} tries.");
+                }
+            } while (inputInt != magicNumber);
+            Console.Write("Would you like to play again? ");
+            answer = Console.ReadLine();
+        } while (answer == "yes");
+
     }
 }
